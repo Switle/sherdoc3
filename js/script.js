2c654a2fe0ee61b13ce9bcdd192ad9a3ce3770c6
Switle <69729725+Switle@users.noreply.github.com>
2023-09-19 17:23:17 +0300

    Finish sections header, footer, main, easy, how

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function burger() {
+	const menu = document.querySelector('.mobile-menu');
+	const burger = document.querySelector('.burger');
+	const mask = document.querySelector('.mask');
+	const body = document.querySelector('body');
+
+	burger.addEventListener('click', () => {
+		menu.classList.toggle('active');
+		burger.classList.toggle('active');
+		mask.classList.toggle('active');
+		body.classList.toggle('noscroll');
+	});
+
+	window.addEventListener('resize', () => {
+		menu.classList.remove('active');
+		burger.classList.remove('active');
+		mask.classList.remove('active');
+		body.classList.remove('noscroll');
+	});
+}
+
+burger();
+
+
+
+//# sourceMappingURL=script.js.map
